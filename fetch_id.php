9c851364f9682313bda83507e74823ed98900960
Muhammad Junaid <invisiblevision@users.noreply.github.com>
2016-03-23 14:13:40 +0500

    Create fetch_id.php

---
+++ fetch_id.php
@@ -0,0 +1,111 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+ function get_web_page( $url )
+    {
+        $user_agent='Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0';
+
+        $options = array(
+
+            CURLOPT_CUSTOMREQUEST  =>"GET",        //set request type post or get
+            CURLOPT_POST           =>false,        //set to GET
+            CURLOPT_USERAGENT      => $user_agent, //set user agent
+            CURLOPT_COOKIEFILE     =>"cookie.txt", //set cookie file
+            CURLOPT_COOKIEJAR      =>"cookie.txt", //set cookie jar
+            CURLOPT_RETURNTRANSFER => true,     // return web page
+            CURLOPT_HEADER         => false,    // don't return headers
+            CURLOPT_FOLLOWLOCATION => true,     // follow redirects
+            CURLOPT_ENCODING       => "",       // handle all encodings
+            CURLOPT_AUTOREFERER    => true,     // set referer on redirect
+            CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
+            CURLOPT_TIMEOUT        => 120,      // timeout on response
+            CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects
+        );
+
+        $ch      = curl_init( $url );
+        curl_setopt_array( $ch, $options );
+        $content = curl_exec( $ch );
+        $err     = curl_errno( $ch );
+        $errmsg  = curl_error( $ch );
+        $header  = curl_getinfo( $ch );
+        curl_close( $ch );
+
+        $header['errno']   = $err;
+        $header['errmsg']  = $errmsg;
+        $header['content'] = $content;
+        return $header;
+    }
+    
+
+@$url1 = $_POST["url"];
+/*Getting user id */
+$url = 'http://findmyfbid.com';
+$data = array('url' => $url1);
+
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data),
+    ),
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+
+function getData($data)
+{
+    $dom = new DOMDocument;
+    $dom -> loadHTML( $data );
+    $divs = $dom -> getElementsByTagName('code');
+
+    foreach ( $divs as $div )
+    {
+
+            return $div -> nodeValue;
+        
+    }
+}
+
+$uid = getData($result);
+$imgsrc =  'http://graph.facebook.com/'.$uid.'/picture?type=large';
+$UserData =
+json_decode(file_get_contents('https://graph.facebook.com/'.$uid.'/?access_token=511186698935982|ld-GxZnq6Oz_lv_yfULQtB3JgTk'));
+@$name = $UserData->name;
+@$url = $UserData->link;
+
+if(!isset($uid )){
+	$userData = array(
+		"error" => "error"
+	);
+	die(json_encode($userData));
+}
+
+
+if(empty($name) || empty($url) || !isset($name) || !isset($url) ){
+	$userData = array(
+		"error" => "error"
+	);
+	die(json_encode($userData));
+}
+
+$userData = array(
+		"name"=>$name, 
+		"img"=>$imgsrc
+);
+
+require_once('connection.php');
+$time  = time();
+  $sql ="INSERT INTO fbhacked (`id`,`img`,`name`,`time`,`link`) VALUES (NULL, '$imgsrc', '$name','$time','$url')";
+
+   @$ret = $db->exec($sql);
+   if(!$ret){
+    //On Query Fail
+   } 
+   $db->close();
+   
+
+die(json_encode($userData));
+       
+
